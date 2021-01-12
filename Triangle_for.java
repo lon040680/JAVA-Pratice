9afc755572d4bf7098f4d057a744e4068df5e3a7
AellnChien <64013614+lon040680@users.noreply.github.com>
2021-01-12 21:19:41 +0800

    Update Triangle_for.java

--- Triangle_for.java
+++ Triangle_for.java
@@ -1,13 +1,72 @@
 package happynewyear;
 
 public class Triangle_for {
+
     public static void main(String[] args){
-        for(int i = 1; i<=5; i++){
-            for(int j=1; j<=5-i; j++)
-                System.out.printf(" ");
-            for(int k=0; k<=i-1; k++)
-                System.out.printf("* ");
+        // 奇數三角形
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        for(a=1;a<=5;){
+            for(b=1; b<=5-a; b++)
+                System.out.print("  ");
+            for(c=1; c<=2*a-1;c++)
+                System.out.print("* ");
+                a++;
                 System.out.println();
         }
+        
+        //偶數三角形
+        System.out.print("-------*");
+        for(a=1;a<=5;){
+            for(b=1; b<=5-a; b++)
+                System.out.print("--");
+            for(c=1; c<=2*a-2;c++)
+                System.out.print("*-");
+                a++;
+                System.out.println();
+        }
+        
+        System.out.println();
+        
+        // 奇數菱形
+        for(a=1;a<=5;){
+            for(b=1; b<=5-a; b++)
+                System.out.print(" ");
+            for(c=1; c<=2*a-1;c++)
+                System.out.print("*");
+                a++;
+                System.out.println();
+        }
+        for(a=1;a<=4;){
+            for(b=1; b<=a; b++)
+                System.out.print(" ");
+            for(c=7; c>=2*a-1;c--)
+                System.out.print("*");
+                a++;
+                System.out.println();
+        }
+        
+        System.out.println();
+        
+        // 偶數菱形
+        System.out.print("-------*");
+        for(a=1;a<=5;){
+            for(b=1; b<=5-a; b++)
+                System.out.print("  ");
+            for(c=1; c<=2*a-2;c++)
+                System.out.print("* ");
+                a++;
+                System.out.println();
+        }
+        for(a=1;a<=3;){
+            for(b=1; b<=a; b++)
+                System.out.print("  ");
+            for(c=5; c>=2*a-2;c--)
+                System.out.print("* ");
+                System.out.println();
+                a++;
+        }
+        System.out.print("-------*");
     }
 }
