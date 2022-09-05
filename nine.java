1363667cb9a33266bce142ac0ef7bafff378b135
AellnChien <64013614+lon040680@users.noreply.github.com>
2022-09-06 04:26:22 +0800

    Update nine.java

--- nine.java
+++ nine.java
@@ -2,8 +2,8 @@ package happynewyear;
 
 public class nine {
     public static void main(String[] args){
-        int i = 2;
-        int j = 1;
+        int i = 1;
+        int j = 2;
         while(i<=9){
             while(j<=9){
                 System.out.printf(i + " X " + j + " =" + (i*j) + "   ");
@@ -13,5 +13,12 @@ public class nine {
             i++;
             j = 1;
         }
+//         for(i=1; i<=9; i++) {
+// 			for(j=2; j<=9; j++) {
+// 				System.out.print(j + "x" + i + "=" + (i*j) + "\t");	
+// 			}
+// 			System.out.println("");
+// 			j = 1;
+// 		}
     }
 }
